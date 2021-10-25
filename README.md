6e7634a0044b118d3ef8c4ad3faa67040b6fbde6
keitarotaylor <keitarotaylor@gmail.com>
2021-10-25 11:25:21 -0400

    functional app with auth

---
+++ README.md
@@ -0,0 +1,1 @@
+Successfully created an app using express with login authentication that's different than savage-auth.
---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://keitarotaylor:nanticoke@cluster0.e2snc.mongodb.net/contactAuth?retryWrites=true&w=majority', 
+    'dbName': 'contactAuth'
+};
